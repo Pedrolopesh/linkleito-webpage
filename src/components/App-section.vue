7a0436bf4613e9b728ef72f07105eac83c9cda9d
Pedro Lopes <pedrolopeshls99@gmail.com>
2020-09-23 02:25:40 -0300

    fix on texts

--- src/components/App-section.vue
+++ src/components/App-section.vue
@@ -13,7 +13,7 @@
                 <h1>O aplicativo Link Leito</h1>
                 
                 <p class="mt-3">
-                    O aplicativo atualmente está em uso pela equipe do Hospital Infantil Joana de Gusmão em florianópolis, as equipes da emergência, NIR e limpeza utilizam o aplicativo atualmente. O app conta com diversas funcionalidades, dês do registro do paciente que precisa ser atendido até a gestão do Hospital como um todo, a ideia inicial que motivou a equipe de desenvolvimento realizar este projeto foi a dificuldade em fazer a gestão do hospital de maneira eficiente e que ajude todos os pacientes e a própria equipe do hospital a realizar atendimento mais eficaz e rapida podendo assim ter uma sincronia com todos os envolvidos do processo que faz o atendimento do paciente. 
+                    O aplicativo atualmente está em uso pela equipe do Hospital Infantil Joana de Gusmão em florianópolis, as equipes da emergência, NIR e limpeza utilizam o aplicativo atualmente na unidade D do hospital. O app conta com diversas funcionalidades, dês do registro do paciente que precisa ser atendido até a gestão do Hospital como um todo, a ideia inicial que motivou a equipe de desenvolvimento a realizar este projeto foi a dificuldade em fazer a gestão do hospital de maneira eficiente e que ajude todos os pacientes e a própria equipe do hospital a realizar atendimento mais eficaz e rápida podendo assim ter uma sincronia com todos os envolvidos do processo que faz o atendimento do paciente. 
                 </p>
             </div>
 
--- src/components/Mission-section.vue
+++ src/components/Mission-section.vue
@@ -27,9 +27,9 @@
 export default {
     data:() => ({
         itens:[
-            {imgSrc: './icons-mission-01.svg', title: 'Como funciona?', description: 'O app conta com o acesso a usuários que fazem parte da equipe do Hospital, uma vez com o acesso ao app, é possivel registrar pacientes, internar, transferir de unidade ou de leito, dar alta e mandar leito para limpesa entre outras utilidades que se complementam.'},
+            {imgSrc: './icons-mission-01.svg', title: 'Como funciona?', description: 'O app conta com o acesso a usuários que fazem parte da equipe do Hospital, no Link Leito é possível registrar pacientes, internar nas unidades cadastradas, transferir para outras unidades ou de outros leitos, dar altas, direcionar os leitos para a equipe de limpeza entre outras utilidades que se complementam.'},
             {imgSrc: './icons-mission-02.svg', title: 'Para quem?', description: 'O aplicativo foi desenvolvido com o intuito de atender a Hospitais que precisam melhorar seus processos tanto o atendimento do paciente e seu acompanhamento e até as equipes que fazem parte do hospital para que todos possam ter uma qualidade e eficiência.'},
-            {imgSrc: './icons-mission-03.svg', title: 'Sobre o projeto', description: 'O Link Leito é um projeto que foi desenvolvido para que tenha um impacto social na vida de todos, nossa missão é fazer com que todos tenham uma melhor experiência no hospital, tanto os pacientes quanto a equipe de profissionais do hospital em questão.'},
+            {imgSrc: './icons-mission-03.svg', title: 'Sobre o projeto', description: 'O Link Leito é um projeto que nasceu a partir da idea de se criar um impacto social na vida de todos, nossa missão é fazer com que todos tenham uma melhor experiência no hospital, tanto os pacientes quanto a equipe de profissionais do hospital em questão.'},
         ]
     })    
 }
